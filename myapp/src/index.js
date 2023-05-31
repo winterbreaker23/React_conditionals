1777dea51f9d1e3e55621628ce1a9c07cce98d0f
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 18:00:36 +0100

    If statement

---
+++ myapp/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+function MissedGoal() {
+  return <h1>MISSED!</h1>;
+}
+
+function MadeGoal() {
+  return <h1>Goal!</h1>;
+}
+function Goal(props){
+  const isGoal=props.isGoal;
+  if(isGoal){
+    return <MadeGoal/>;
+  }else{
+    return <MissedGoal/>;
+  }
+}
+root.render(
+  <Goal isGoal={false}/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
